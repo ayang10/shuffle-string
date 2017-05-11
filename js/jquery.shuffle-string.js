d8d56537a7a0944e1548fdd3d854ec51c1c24db3
ayang10 <ayang014@gmail.com>
2017-05-11 15:49:10 -0400

    first commit

---
+++ js/jquery.shuffle-string.js
@@ -0,0 +1,34 @@
+(function($) {
+    $.fn.shuffleString = function (options) { // qjuery way of defining functions
+
+        var settings = $.extend({
+            color: "#0000", // default color of my text
+            done: null
+        }, options)
+
+
+        return this.each(function() {
+            $(this).text(shuffleString($(this).text()));
+            $(this).css('color', settings.color);
+
+            if($.isFunction(settings.done)){
+                settings.done.call(this);
+            }
+
+            function shuffleString(str) {
+                var array = str.split("");
+                for(var index = array.length - 1; index > 0; index--){
+                    var val = Math.floor(Math.random() * (index + 1));
+                    var temp = array[index];
+                    array[index] = array[val];
+                    array[val] = temp;
+                }
+                return array.join("");
+
+
+            }
+            
+        })
+    }
+
+}(jQuery)); // wrapped inside this function because anything not wrap in this function may conflict with functions outside of the plugin
---
+++ js/script.js
@@ -0,0 +1,6 @@
+$("h1").shuffleString({
+    color: "#42f4d9",
+    done: function() {
+        console.log("Shuffle has been completed");
+    }
+});
